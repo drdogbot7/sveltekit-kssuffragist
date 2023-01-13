a7e084e7a371ee9f9f14b14be7d5a1f915321145
Jeremy Mullis <jeremy.mullis@gmail.com>
2023-01-13 16:28:02 -0600

    add page stubs, nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,32 @@
 	import logo from '$lib/assets/logo.svg';
 </script>
 
-<a href="/">
-	<img class="max-w-xs mx-auto" src={logo} alt="Kansas Suffragist Memorial logo" />
-</a>
+<header>
+	<div class="sm:flex justify-center gap-12">
+		<a href="/">
+			<img class="w-64 mx-auto" src={logo} alt="Kansas Suffragist Memorial logo" />
+		</a>
+		<nav class="flex gap-x-6 gap-y-4 justify-center items-center mt-4 flex-wrap">
+			<a
+				class="text-blue-500 w-full sm:w-auto text-center hover:underline font-bold uppercase"
+				href="/history">History</a
+			>
+			<a
+				class="text-blue-500 w-full sm:w-auto text-center hover:underline font-bold uppercase"
+				href="/rfq">Request for Qualifications</a
+			>
+			<a
+				class="text-blue-500 w-full sm:w-auto text-center hover:underline font-bold uppercase"
+				href="/submit">Submit</a
+			>
+			<a
+				class="text-blue-500 w-full sm:w-auto text-center hover:underline font-bold uppercase"
+				href="/give">Give</a
+			>
+		</nav>
+	</div>
+</header>
 
-<div class="container max-w-screen-lg mx-auto">
+<div class="container max-w-screen-lg mx-auto px-2">
 	<slot />
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				blue: {
+					DEFAULT: '#0C569D',
+					50: '#C2DFFA',
+					100: '#A6D0F8',
+					200: '#6DB2F4',
+					300: '#3494EF',
+					400: '#1075D6',
+					500: '#0C569D',
+					600: '#0A4985',
+					700: '#083C6E',
+					800: '#072F56',
+					900: '#05223E'
+				}
+			}
+		}
 	},
 	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')]
 };
